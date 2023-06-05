a985f9d0a72c3df7376e6a4585548911f995d87d
YelloElefant <alextrotternz@gmail.com>
2023-06-05 21:37:29 +1200

    fixing issues
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- MockUp/rules.js
+++ MockUp/rules.js
@@ -6,6 +6,7 @@ function openTab(ele) {
    if (ele.id.includes("open")) {
       for (var i = 0; i < ele.children.length; i++) {
          ele.children[i].style.display = "none";
+         ele.children[i].style.opacity = "0";
       }
       ele.id = ele.id.replace("open", "close");
       return;
@@ -14,6 +15,7 @@ function openTab(ele) {
    if (ele.id.includes("close")) {
       for (var i = 0; i < ele.children.length; i++) {
          ele.children[i].style.display = "block";
+         ele.children[i].style.opacity = "1";
       }
       ele.id = ele.id.replace("close", "open");
    }
