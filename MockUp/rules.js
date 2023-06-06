63057cdfae71096ce2c0408e988766a2eddb9db4
9atrotte <9atrotte@hbhs.school.nz>
2023-06-07 10:36:07 +1200

    adding a slight background and boarder

--- MockUp/rules.js
+++ MockUp/rules.js
@@ -7,17 +7,17 @@ function openTab(ele) {
       for (var i = 0; i < ele.children.length; i++) {
          ele.children[i].style.display = "none";
          ele.children[i].style.opacity = "0";
-         ele.style.backgroundColor = "aqua";
+         // ele.style.backgroundColor = "aqua";
       }
       ele.id = ele.id.replace("open", "close");
       return;
    }
-   
+
    if (ele.id.includes("close")) {
       for (var i = 0; i < ele.children.length; i++) {
          ele.children[i].style.display = "block";
          ele.children[i].style.opacity = "1";
-         ele.style.backgroundColor = "grey";
+         // ele.style.backgroundColor = "grey";
       }
       ele.id = ele.id.replace("close", "open");
    }
