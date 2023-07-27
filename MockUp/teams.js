826f5c658c3c189b6e2faa7492c6cd88d2fd598a
YelloElefant <alextrotternz@gmail.com>
2023-07-27 17:06:23 +1200

    fixing sintax problems
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- MockUp/teams.js
+++ MockUp/teams.js
@@ -2,15 +2,17 @@ console.log("js loaded")
 
 function showMore(ele) {
     let content = ele.previousSibling.previousSibling;
+    let img = content.previousSibling.previousSibling.children[0].children[0];
+    console.log(img)
     console.log(content)
     if (content.style.display == "none") {
         content.style.display = "block"
         ele.children[0].children[0].style.transform = "rotate(180deg)";
-
-    }
-    else {
+        img.style.height = "100px";
+    } else {
         content.style.display = "none"
         ele.children[0].children[0].style.transform = "rotate(0deg)";
+        img.style.height = "100%";
 
     }
 
