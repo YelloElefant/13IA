6ec449cfb386b1f24c07f3b2a380727696c9984c
9atrotte <104942752+9atrotte@users.noreply.github.com>
2023-08-02 01:29:38 +0000

    fixing file direction

--- Release/footer.js
+++ Release/footer.js
@@ -2,7 +2,7 @@ console.log("footer loading...")
 fetchStuff()
 
 async function fetchStuff() {
-    const response = await fetch("http://localhost/9atrotte/13IA/MockUp/footer.html", {
+    const response = await fetch("http://localhost/9atrotte/13IA/Release/footer.html", {
         mode: 'no-cors',
         method: "get",
         headers: {
