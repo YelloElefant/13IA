46da70b590f87bb59464c2e6b4d24abe9b538523
YelloElefant <alextrotternz@gmail.com>
2023-08-02 17:51:34 +1200

    starting the teams card script
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Scripts/tableMaker/tableMaker/Program.cs
+++ Scripts/tableMaker/tableMaker/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using FileStuff;
 
 namespace TableMaker;
 
@@ -23,10 +18,10 @@ public class TableMaker
       filePathTeams2 = args[1];
       string time = args[2];
 
-      ReadFiles(filePathTeams1, teams1);
-      ReadFiles(filePathTeams2, teams2);
+      FileStuff.FileStuff.ReadFiles(filePathTeams1, teams1);
+      FileStuff.FileStuff.ReadFiles(filePathTeams2, teams2);
 
-      using(StreamWriter sw = new StreamWriter(@"./output.txt")) 
+      using (StreamWriter sw = new StreamWriter(@"./output.txt"))
       {
          sw.WriteLine("<tbody>");
          for (int i = 0; i < teams1.Count; i++)
@@ -47,17 +42,6 @@ public class TableMaker
 
    }
 
-   public static void ReadFiles(string filePath, List<string> teamList) 
-   { 
-      using(StreamReader reader = new StreamReader(filePath)) 
-      {
-         while (!reader.EndOfStream)
-         {
-            var line = reader.ReadLine();
-            teamList.Add(line);
-         }
-      }
-   
-   }
+
 
 }
