d575c037ad787d4bfbaedbbadbcb2e442409b427
9atrotte <104942752+9atrotte@users.noreply.github.com>
2023-08-02 01:34:03 +0000

    fixing public ip

--- Release/footer.js
+++ Release/footer.js
@@ -2,7 +2,7 @@ console.log("footer loading...")
 fetchStuff()
 
 async function fetchStuff() {
-    const response = await fetch("http://localhost/9atrotte/13IA/Release/footer.html", {
+    const response = await fetch("https://118.95.173.58/9atrotte/13IA/Release/footer.html", {
         mode: 'no-cors',
         method: "get",
         headers: {
