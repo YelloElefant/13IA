6020ded824df6b614b0b4cadb7a62545d7b7e2e2
YelloElefant <alextrotternz@gmail.com>
2023-08-17 22:21:44 +1200

    adding Don and Pa
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Release/scripts/maps.js
+++ Release/scripts/maps.js
@@ -19,21 +19,57 @@ activePlace.style.color = "var(--color-gold)";
 //document.getElementById('activeDay').style.display = "table";
 
 
-function setActivePlace(ele, evt, dayName) {
+function setActivePlace(ele, evt, placeName) {
    //removing the active day
    activePlace = document.getElementById("activePlace");
    activePlace.style.border = "solid white thin";
    activePlace.style.borderRight = "solid white";
+
    activePlace.style.color = "black";
    activePlace.id = "";
 
+   console.log("placeName: " + placeName);
+
    ele.id = "activePlace";
+   if (placeName == "pa" || placeName == "don") {
 
-   ele.style.transition = "border-color 0.5s ease-in-out";
-   ele.style.borderRight = "solid black";
-   ele.style.color = "var(--color-gold)";
-
+      ele.style.transition = "border-color 0.5s ease-in-out";
+      ele.style.borderTop = "solid black";
+      ele.style.color = "var(--color-gold)";
+      console.log("true")
+   } else {
+      ele.style.transition = "border-color 0.5s ease-in-out";
+      ele.style.borderRight = "solid black";
+      ele.style.color = "var(--color-gold)";
+      console.log("flase")
+   }
    let mapIframe = document.getElementById("mapIframe")
-   mapIframe.src = "https://www.google.com/maps/embed/v1/place?q=" + placesMap.get(dayName) + "&key=AIzaSyA_tMXSrrQSrdKlEB7P3_Wo8U9fq0Htn3A";
+   mapIframe.src = "https://www.google.com/maps/embed/v1/place?q=" + placesMap.get(placeName) + "&key=AIzaSyA_tMXSrrQSrdKlEB7P3_Wo8U9fq0Htn3A";
+
+
+   if (placeName == "pa" || placeName == "don") {
+      return;
+   }
+
+   let foldOut = document.getElementById("foldOut");
+   let foldOutDivs = foldOut.getElementsByTagName("div");
+   if (placeName == "uni") {
+      foldOut.style.display = "flex";
+      foldOut.style.backgroundColor = "#f2f2f2"
+      foldOutDivs[0].style.backgroundColor = "white";
+      foldOutDivs[1].style.backgroundColor = "white";
+      foldOutDivs[0].style.color = "black";
+      foldOutDivs[1].style.color = "black";
+
+   } else {
+      foldOut.style.color = "transparent";
+      foldOut.style.backgroundColor = "transparent";
+      foldOutDivs[0].style.backgroundColor = "transparent";
+      foldOutDivs[1].style.backgroundColor = "transparent";
+      foldOutDivs[0].style.color = "transparent";
+      foldOutDivs[1].style.color = "transparent";
+      foldOutDivs[0].style.border = "none";
+      foldOutDivs[1].style.border = "none";
+   }
 
 }
