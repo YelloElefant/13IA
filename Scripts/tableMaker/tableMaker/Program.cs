2fbe71c020b7c618e6f2246dfccf154a494f25ae
YelloElefant <alextrotternz@gmail.com>
2023-08-21 13:21:28 +1200

    making table maker do team managemnt
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Scripts/tableMaker/tableMaker/Program.cs
+++ Scripts/tableMaker/tableMaker/Program.cs
@@ -8,37 +8,53 @@ public class TableMaker
    public static string filePathTeams2;
    const string tableV = "<td class=\"tableV\">v</td>";
 
-   public static List<string> teams1 = new List<string>();
+   public static List<string> coaches = new List<string>();
 
-   public static List<string> teams2 = new List<string>();
+   public static List<string> managers = new List<string>();
+   public static List<string> acommidation = new List<string>();
 
    static void Main(string[] args)
    {
       filePathTeams1 = args[0];
-      filePathTeams2 = args[1];
-      string time = args[2];
+      //filePathTeams2 = args[1];
+      //string time = args[2];
+      var file = new StreamReader(filePathTeams1).ReadToEnd(); // big string
+      var lines = file.Split(new char[] { '\n' });           // big array
+      var count = lines.Count();                               // big number
 
-      FileStuff.FileStuff.ReadFiles(filePathTeams1, teams1);
-      FileStuff.FileStuff.ReadFiles(filePathTeams2, teams2);
+
 
       using (StreamWriter sw = new StreamWriter(@"./output.txt"))
       {
-         sw.WriteLine("<tbody>");
-         for (int i = 0; i < teams1.Count; i++)
+         for (int i = 0; i < count; i++)
          {
-            sw.WriteLine("<tr>");
+            var returned = FileStuff.FileStuff.ReadFiles(filePathTeams1, coaches, managers, acommidation);
+            coaches = returned[0];
+            managers = returned[1];
+            acommidation = returned[2];
+            foreach (var coach in coaches)
+            {
+               sw.WriteLine("<tr>");
+               sw.WriteLine($"<td>Coach</td>");
+               sw.WriteLine($"<td>{coach}</td>");
+               sw.WriteLine("</tr>");
+            }
+            foreach (var manager in managers)
+            {
+               sw.WriteLine("<tr>");
+               sw.WriteLine($"<td>Manager</td>");
+               sw.WriteLine($"<td>{manager}</td>");
+               sw.WriteLine("</tr>");
+            }
+            foreach (var acommidation in acommidation)
+            {
+               sw.WriteLine("<tr>");
+               sw.WriteLine($"<td>Acommidation</td>");
+               sw.WriteLine($"<td>{acommidation}</td>");
+               sw.WriteLine("</tr>");
+            }
 
-            sw.WriteLine($"<td> {i + 1} </td>");
-            sw.WriteLine("<td> pool </td>");
-
-            sw.WriteLine("<td class=\"teamLeft\">" + teams1[i] + "</td>");
-            sw.WriteLine(tableV);
-            sw.WriteLine("<td class=\"teamRight\">" + teams2[i] + "</td>");
-            sw.WriteLine($"<td> {time} </td>");
-            sw.WriteLine("<td> VENUE </td>");
-            sw.WriteLine("</tr>");
          }
-         sw.WriteLine("</tbody>");
       }
 
    }
--- Scripts/tableMaker/tableMaker/files.cs
+++ Scripts/tableMaker/tableMaker/files.cs
@@ -4,14 +4,20 @@ public static class FileStuff
 {
 
 
-   public static void ReadFiles(string filePath, List<string> teamList)
+   public static List<List<string>> ReadFiles(string filePath, List<string> coaches, List<string> managers, List<string> acommidation)
+
    {
       using StreamReader reader = new(filePath);
       while (!reader.EndOfStream)
       {
          var line = reader.ReadLine();
-         teamList.Add(line);
+         var values = line.Split(';');
+         coaches = values[0].Split(',').ToList();
+         managers = values[1].Split(',').ToList();
+         acommidation = values[2].Split(',').ToList();
       }
+
+      return new List<List<string>> { coaches, managers, acommidation };
 
    }
 }
