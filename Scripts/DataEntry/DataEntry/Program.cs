69e9f4eee2b51a343a432987103b314a7e2a889d
YelloElefant <alextrotternz@gmail.com>
2023-08-28 21:59:53 +1200

    finished pool points inputer
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Scripts/DataEntry/DataEntry/Program.cs
+++ Scripts/DataEntry/DataEntry/Program.cs
@@ -48,16 +48,72 @@ class DataEntry
             wdl2[1] = 1;
          }
 
-         teams.Add(new Team(splitLine[1], splitLine[2], splitLine[6], splitLine[7], wdl1));
 
 
 
-         teams.Add(new Team(splitLine[8], splitLine[9], splitLine[13], splitLine[14], wdl2));
+         teams.Add(new Team(splitLine[1], splitLine[2], splitLine[6], splitLine[7], wdl1, splitLine[9]));
+
+
+
+         teams.Add(new Team(splitLine[8], splitLine[9], splitLine[13], splitLine[14], wdl2, splitLine[2]));
 
 
 
       }
 
+      //reading old points
+
+      string oldPointsPath = args[1];
+      var oldStuffLines = ReadFiles.ReadHTML(oldPointsPath);
+
+      for (int i = 0; i < oldStuffLines.Count; i++)
+      {
+         for (int j = 0; j < teams.Count; j++)
+         {
+            if (teams[j].Name.ToLower() == oldStuffLines[i][0].ToLower())
+            {
+               teams[j].OldPF = int.Parse(oldStuffLines[i][4]);
+               teams[j].OldPA = int.Parse(oldStuffLines[i][5]);
+               teams[j].OldBonusPoints = int.Parse(oldStuffLines[i][6]);
+               teams[j].OldPoints = int.Parse(oldStuffLines[i][7]);
+               teams[j].oldWDL = new int[3];
+               teams[j].oldWDL[0] = int.Parse(oldStuffLines[i][1]);
+               teams[j].oldWDL[1] = int.Parse(oldStuffLines[i][2]);
+               teams[j].oldWDL[2] = int.Parse(oldStuffLines[i][3]);
+
+            }
+         }
+      }
+
+
+
+      //calculating new points
+
+      for (int i = 0; i < teams.Count; i++)
+      {
+         teams[i].Points = teams[i].OldPoints + teams[i].Points;
+         teams[i].PF = teams[i].OldPF + teams[i].PF;
+         teams[i].PA = teams[i].OldPA + teams[i].PA;
+         teams[i].BonusPoints = teams[i].OldBonusPoints + teams[i].BonusPoints;
+         for (int j = 0; j < 3; j++) { teams[i].WDL[j] += teams[i].oldWDL[j]; }
+      }
+
+      //making html file
+      using StreamWriter file = new("./output.html");
+      foreach (var team in teams)
+      {
+         file.WriteLine(@"<tr class=""tablePoints"">");
+         file.WriteLine(@"<td>" + team.Name + "</td>");
+         foreach (var item in team.WDL)
+         {
+            file.WriteLine(@"<td>" + item + "</td>");
+         }
+         file.WriteLine(@"<td>" + team.PF + "</td>");
+         file.WriteLine(@"<td>" + team.PA + "</td>");
+         file.WriteLine(@"<td>" + team.BonusPoints + "</td>");
+         file.WriteLine(@"<td>" + team.WinPoints + "</td>");
+         file.WriteLine("</tr>");
+      }
 
 
       Console.WriteLine(teams);
--- Scripts/DataEntry/DataEntry/TeamInfo.cs
+++ Scripts/DataEntry/DataEntry/TeamInfo.cs
@@ -4,23 +4,29 @@ namespace DataEntry
 {
    class Team
    {
-      public string TeamName { get; set; }
-      public int NewScore { get; set; }
-
-      public int WinPoints { get; set; }
+      public string Name { get; set; }
+      public int PF { get; set; }
       public int BonusPoints { get; set; }
-
-      public int OldScore { get; set; }
-
+      public int WinPoints { get; set; }
+      public int PA { get; set; }
       public int[] WDL { get; set; }
 
-      public Team(string teamName, string newScore, string bonusPoints, string winPoints, int[] wdl)
+
+      public int Points { get; set; }
+      public int OldPoints { get; set; }
+      public int OldBonusPoints { get; set; }
+      public int OldPF { get; set; }
+      public int OldPA { get; set; }
+      public int[] oldWDL = new int[3] { 0, 0, 0 };
+      public Team(string teamName, string Score, string bonusPoints, string winPoints, int[] wdl, string pa)
       {
-         TeamName = teamName;
-         NewScore = int.Parse(newScore);
+         Name = teamName;
+         PF = int.Parse(Score);
          WinPoints = int.Parse(winPoints);
          BonusPoints = int.Parse(bonusPoints);
          WDL = wdl;
+         PA = int.Parse(pa);
+         Points = WinPoints + BonusPoints;
       }
 
 
